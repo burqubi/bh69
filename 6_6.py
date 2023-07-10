3ac8ef9310773394085e40760beb46b5a0a37610
Илья <zaz@>
2023-07-10 16:07:41 +0300

    dz

---
+++ 6_6.py
@@ -0,0 +1,7 @@
+a = [7, 8, 32, 1, 13, 43, 55, 221, 6, 16, 18, 22, 99, 98]
+even = [i for i in a if i % 2 == 0]
+odd = [i for i in a if i % 2 == 1]
+sort = even + odd
+# print(B)
+# print(C)
+print(sort)
