3a077eba354cece2b6ee2f2932aee9221d62dc18
Илья <zaz@>
2023-08-09 18:25:07 +0300

    hw

--- 12_1.py
+++ 12_1.py
@@ -19,8 +19,22 @@ class Birthdays(Base):
     __tablename__ = 'birthdays'
     id = Column(INT, primary_key=True)
     cel_name = Column(VARCHAR(64), nullable=False)
-    date_of_birth = Column(INT, nullable=False)
+    date_of_birth = Column(VARCHAR(10), nullable=False)
     date_created = Column(TIMESTAMP, default=datetime.utcnow())
     date_updated = Column(TIMESTAMP, onupdate=datetime.utcnow())
     date_id = Column(VARCHAR(64), ForeignKey('dates.id', onupdate='CASCADE'), nullable=False)
     date = relationship(Dates, back_populates='dates')
+
+# def update(new_date, new_events, new_description):
+#     Dates.date.__add__(new_date)
+#     Dates.events.__add__(new_events)
+#     Dates.description.__add__(new_description)
+
+def update(values):
+    for i in range(len(values)):
+        new_date = values[0]
+        new_events = values[1]
+        new_description = values[2]
+        Dates.date.__add__(new_date)
+        Dates.events.__add__(new_events)
+        Dates.description.__add__(new_description)
